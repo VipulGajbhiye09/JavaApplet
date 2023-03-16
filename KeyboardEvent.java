fcdd619c5289578a6ff243075b2c793c66a35428
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-03-16 23:48:03 +0530

    Update KeyboardEvent.java

--- KeyboardEvent.java
+++ KeyboardEvent.java
@@ -1,1 +1,22 @@
+import java.applet.*;
+import java.awt.*;
+import java.awt.event.*;
 
+// To demonstrate the working of Keyboard Events
+public class KeyboardEvents extends Applet implements KeyListener ,ActionListener {
+Label purpose;
+char keyChar = ' ';
+int number = 0;
+String string1 = " ", string2 = " ", string3 = " ", string4 = " ";
+Button start;
+{
+requestFocus(); // should make the applet the active component for Key
+events
+start = new Button("START");
+add(start);
+start.addActionListener(this);
+addKeyListener(this); 
+
+purpose = new Label("The purpose of this program is to show how Keyboard Events work");
+add(purpose);
+}

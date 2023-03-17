c32abcdb2304ddb131b2e7461256f885dcbaaa37
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-03-17 23:34:31 +0530

    Update KeyboardEvent.java

--- KeyboardEvent.java
+++ KeyboardEvent.java
@@ -20,3 +20,17 @@ addKeyListener(this);
 purpose = new Label("The purpose of this program is to show how Keyboard Events work");
 add(purpose);
 }
+public void keyPressed(KeyEvent e)
+{
+  number++; 
+  string1 = e.getKeyText(e.getKeyCode()); // show which key is pressed
+  if(e.getKeyCode() == e.VK_UP) // check if UP arrow key is pressed
+  string2 = "Up Arrow key";
+  if(e.getKeyCode() == e.VK_DOWN) // check if DOWN arrow key is pressed
+  string2 = "Down Arrow key";
+  if(e.getKeyCode() == e.VK_LEFT) // check if LEFT arrow key is pressed
+  string2 = "Left Arrow key";
+  if(e.getKeyCode() == e.VK_RIGHT) // check if Right arrow key is pressed
+  string2 = "Right Arrow key";
+  if(e.getKeyCode() == e.VK_ENTER) // check if ENTER key is pressed
+ 

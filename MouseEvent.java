896aa376ce9a8d9e6e293572034cff25a86d2166
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-03-18 23:02:51 +0530

    Update MouseEvent.java

--- MouseEvent.java
+++ MouseEvent.java
@@ -1,1 +1,10 @@
+import java.awt.*;
+import java.awt.event.*;
+
+public class MouseListenerExample extends Frame implements MouseListener{
+  Label l;
+  MouseListenerExample(){
+  addMouseListener(this);
+  l=new Label();
+  l.setBounds(20,50,100,20);
 

c34bdab11f515b83b322670d2c8d852364811fc7
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-03-18 23:03:45 +0530

    Update MouseEvent.java

--- MouseEvent.java
+++ MouseEvent.java
@@ -7,4 +7,17 @@ public class MouseListenerExample extends Frame implements MouseListener{
   addMouseListener(this);
   l=new Label();
   l.setBounds(20,50,100,20);
-
+  setSize(300,300);
+  setLayout(null);
+  setVisible(true);
+}
+  
+public void mouseClicked(MouseEvent e) {
+  l.setText("Mouse Clicked");
+}
+public void mouseEntered(MouseEvent e) {
+  l.setText("Mouse Entered");
+}
+public void mouseExited(MouseEvent e) {
+  l.setText("Mouse Exited");
+}

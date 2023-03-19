4a3b4497f26b6e1e8152e3736b1d750434846b0d
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-03-19 23:04:18 +0530

    Update SwingDemo.java

--- SwingDemo.java
+++ SwingDemo.java
@@ -1,1 +1,10 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+public class StudentResult {
+// Function to write a student information in JFrame and storing it in a file
+public static void StudentInfo()
+{
+JFrame f = new JFrame("Student Result Form");
 

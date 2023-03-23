a6fef622c6f2cd64d2cc299db6b5f59f78c60b1a
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-03-23 23:53:16 +0530

    Create gui.java

---
+++ gui.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class StudentResult {
+  
+// Function to write a student information in JFrame and storing it in a file
+public static void StudentInfo()
+{
+  
+JFrame f = new JFrame("Student Result Form");  // Creating a new frame using JFrame
+JLabel l1, l2, l3, l4, l5,l6,l7,l8,l9;
+JTextField t1, t2, t3,t4,t5,t6,t7;
+JComboBox j1, j2;
+JButton b1, b2;
